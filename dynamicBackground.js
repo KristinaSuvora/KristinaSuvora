50f67d570dac9315b13642d1b9666a7e9d182e92
KristinaSuvora <137099687+KristinaSuvora@users.noreply.github.com>
2025-01-24 15:26:12 +0300

    Delete dynamicBackground.js

--- dynamicBackground.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const favoriteColor = localStorage.getItem('favoriteColor');
-    if (favoriteColor) {
-        document.body.style.backgroundColor = favoriteColor;
-    }
-});
