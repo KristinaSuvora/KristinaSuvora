63c74aed9a30244e89684d6ddec190f80117e724
KristinaSuvora <137099687+KristinaSuvora@users.noreply.github.com>
2025-01-24 12:22:33 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,23 +7,9 @@ document.querySelectorAll('.color-option').forEach(option => {
     });
 });
 
-document.querySelectorAll('.avatar-option').forEach(option => {
-    option.addEventListener('click', function() {
-        document.querySelectorAll('.avatar-option').forEach(o => o.classList.remove('selected'));
-        this.classList.add('selected');
-        document.getElementById('avatar').value = this.getAttribute('data-avatar');
-    });
-});
-
 document.getElementById('registrationForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    const name = document.getElementById('name').value;
     const favoriteColor = document.getElementById('favoriteColor').value;
-    const avatar = document.getElementById('avatar').value;
-    const dream = document.getElementById('dream').value;
-    localStorage.setItem('name', name);
     localStorage.setItem('favoriteColor', favoriteColor);
-    localStorage.setItem('avatar', avatar);
-    localStorage.setItem('dream', dream);
     window.location.href = 'main.html';
 });
