892bff816bb4c47d0e4fc41413965a2c6165fbda
KristinaSuvora <137099687+KristinaSuvora@users.noreply.github.com>
2025-01-24 12:33:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
-document.querySelectorAll('.color-option').forEach(option => {
-    option.addEventListener('click', function() {
-        document.querySelectorAll('.color-option').forEach(o => o.classList.remove('selected'));
-        this.classList.add('selected');
-        document.getElementById('favoriteColor').value = this.getAttribute('data-color');
-        document.body.style.backgroundColor = this.getAttribute('data-color');
+document.addEventListener('DOMContentLoaded', function () {
+    // Выбор аватара
+    document.querySelectorAll('.avatar-option').forEach(option => {
+        option.addEventListener('click', function () {
+            document.querySelectorAll('.avatar-option').forEach(o => o.classList.remove('selected'));
+            this.classList.add('selected');
+            document.getElementById('avatar').value = this.getAttribute('data-avatar');
+        });
     });
-});
 
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const favoriteColor = document.getElementById('favoriteColor').value;
-    localStorage.setItem('favoriteColor', favoriteColor);
-    window.location.href = 'main.html';
+    // Выбор цвета
+    document.querySelectorAll('.color-option').forEach(option => {
+        option.addEventListener('click', function () {
+            document.querySelectorAll('.color-option').forEach(o => o.classList.remove('selected'));
+            this.classList.add('selected');
+            document.getElementById('favoriteColor').value = this.getAttribute('data-color');
+            document.body.style.backgroundColor = this.getAttribute('data-color');
+        });
+    });
+
+    // Отправка формы
+    document.getElementById('registrationForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const favoriteColor = document.getElementById('favoriteColor').value;
+        const avatar = document.getElementById('avatar').value;
+        localStorage.setItem('favoriteColor', favoriteColor);
+        localStorage.setItem('avatar', avatar);
+        window.location.href = 'main.html';
+    });
 });
