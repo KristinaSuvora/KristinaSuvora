eec897e8bd5d9b210e0c36b5c05ead8401857dc1
KristinaSuvora <137099687+KristinaSuvora@users.noreply.github.com>
2025-01-24 04:04:27 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
+    // Get user input
+    const name = document.getElementById('name').value;
+    const favoriteColor = document.getElementById('favoriteColor').value;
+    const age = document.getElementById('age').value;
+    const dream = document.getElementById('dream').value;
+
+    // Save to localStorage (to simulate saving user preferences)
+    localStorage.setItem('name', name);
+    localStorage.setItem('favoriteColor', favoriteColor);
+    localStorage.setItem('age', age);
+    localStorage.setItem('dream', dream);
+
+    // Redirect to the main page
+    window.location.href = 'main.html';
+});
